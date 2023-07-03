376c976d45b9679aaeae40eb8d1c2c21e24a852a
Victor Suarez <victorsuarez0507@gmail.com>
2023-07-02 21:07:00 -0500

    Se crea la rama project-2

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -2,15 +2,7 @@ export default function Task () {
     
     return(
         <form>
-            <input type="checkbox"/>Tarea 1
-            <br/>
-            <input type="checkbox"/>Tarea 2
-            <br/>
-            <input type="checkbox"/>Tarea 3
-            <br/>
-            <input type="checkbox"/>Tarea 4
-            <br/>
-            <input type="checkbox"/>Tarea 5  
+                         
         </form>
     );
 }
